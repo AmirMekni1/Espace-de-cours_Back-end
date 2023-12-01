efab37d75d1fcf79e0204d17e0bb00f03d71e1ad
AmirMekni1 <mekniamir09@gmail.com>
2023-12-01 20:19:37 +0100

    2023/12/01 08:19 PM

--- Route/R_Enseignant.js
+++ Route/R_Enseignant.js
@@ -4,95 +4,96 @@ const N_Enseignant = require("../Models/Enseignant");
 const mult = require("multer");
 const cryptage = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
 //--------------------------------------------------------------
 
 photoname = "";
-const mystorge=mult.diskStorage({
-    destination : './Images',
-    filename : (req,photo,redirect)=>{
+const mystorge = mult.diskStorage({
+    destination: './Images',
+    filename: (req, photo, redirect) => {
         L_date = Date.now();
-        let f1 = L_date+"."+photo.mimetype.split('/')[1];
-        redirect(null,f1);
-        photoname = f1 ;
+        let f1 = L_date + "." + photo.mimetype.split('/')[1];
+        redirect(null, f1);
+        photoname = f1;
     }
 });
 
-const upload = mult({storage : mystorge});
+const upload = mult({ storage: mystorge });
 
 
-VerifierToken = (req,res,next)=>{
-    let token = req.headers.autorization;
-    if (!token){
-        res.send("Acces Rejected !!!")
-    }
-    try {
-        jwt.verify(token,"24884920");
-        next();
-    } catch (error) {
-        res.send(error)
-    }
+VerifierToken = (req, res, next) =>
+ { let token = req.headers.authorization;
+     if (!token) {
+         res.send(" Acces rejected  !!! Bara ched darkom ")
+         } try {
+             jwt.verify(token, user.Mot_De_Pass);
+              next(); 
+            } catch (error) {
+                 res.status(500).send(error) 
+                } }
+                
+//_____________________________________________________________________________________________________
+
+router_Enseignant.post("/InscriptionEnseignant", upload.any('img'), (req, res) => {
+
+    data = req.body;
+    cle = cryptage.genSaltSync(10);
+    passwordCrypter = cryptage.hashSync(data.Mot_De_Pass, cle);
+    data.Mot_De_Pass = passwordCrypter;
+    po = new N_Enseignant(data);
+    po.image = photoname;
+    po.save().then(() => {
+        photoname = "";
+        res.status(200).send(po);
+    }).catch(() => {
+        res.status(400).send(error);
+    });
+
 }
-router_Enseignant.post("/InscriptionEnseignant", upload.any('img'), (req,res)=>{
-    
-        data = req.body;
-        cle = cryptage.genSaltSync(10);
-        passwordCrypter = cryptage.hashSync("data.Mot_De_Pass",cle);
-        data.Mot_De_Pass = passwordCrypter ;
-        po = new N_Enseignant(data);
-        po.image = photoname ;
-        po.save().then(()=>{
-                photoname = "";
-                res.status(200).send(po);
-        }).catch(()=>{
-                 res.status(400).send(error);
-        });
-        
-    }
 );
 
-router_Enseignant.post("/SignIn",(req,res)=>{
+router_Enseignant.post("/SignIn", (req, res) => {
     data = req.body;
-  
-    
+
+
 });
 
 
-router_Enseignant.get("/Lister",VerifierToken,(req,res)=>{
-    N_Enseignant.find().then((result)=>{
+router_Enseignant.get("/Lister", VerifierToken, (req, res) => {
+    N_Enseignant.find().then((result) => {
         res.send(result);
-    }).catch(()=>{
+    }).catch(() => {
         res.send('error');
     });
 });
 
 
- router_Enseignant.post("/Connexion", async (req,res)=>{
+router_Enseignant.post("/SIGN", async (req, res) => {
     data = req.body;
-   user = await N_Enseignant.findOne({Email : data.Email})
-    if (!user){
+    user = await N_Enseignant.findOne({ Email: data.Email })
+    if (!user) {
         res.status(401).send("Email Icorrect");
-    }else{
-        cryptage.compare(data.Mot_De_Pass,user.Mot_De_Pass).then((Passe)=>{
+    } else {
+        verifPass = cryptage.compareSync(data.Mot_De_Pass, user.Mot_De_Pass);
+        if (!verifPass) {
+            res.status(402).send("Password Icorrect");
+        } else {
+            payload = {
+                id: user._id,
+                NomPrenom: user.NomPrenom,
+                image: user.image
 
-            if(!Passe){
-                res.send("Mot de Passe Icorrect");
-            }else{
-                payload = {
-                    __id : user._id ,
-                    email : user.Email ,
-                    motdepasse : user.NomPrenom
-                }
-               let userToken = jwt.sign(payload,'24884920');
-                res.status(200).send({mytoken : userToken})
             }
-
-        });
+            tokenE = jwt.sign(payload, user.Mot_De_Pass, { expiresIn: "1h" });
+            res.status(200).send({ MyToken: tokenE })
+        }
     }
 });
-    
-   
 
 
-router_Enseignant.put("/Modifier",(req,res)=>{});
-router_Enseignant.delete("/Supprimer",(req,res)=>{});
-module.exports=router_Enseignant;
+
+
+
+router_Enseignant.put("/Modifier", (req, res) => { });
+router_Enseignant.delete("/Supprimer", (req, res) => { });
+module.exports = router_Enseignant;
--- Route/R_Etudiant.js
+++ Route/R_Etudiant.js
@@ -4,95 +4,98 @@ const N_Etudiant = require("../Models/Etudiant");
 const mult = require("multer");
 const cryptage = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
 //--------------------------------------------------------------
 
 photoname = "";
-const mystorge=mult.diskStorage({
-    destination : './Images',
-    filename : (req,photo,redirect)=>{
+const mystorge = mult.diskStorage({
+    destination: './Images',
+    filename: (req, photo, redirect) => {
         L_date = Date.now();
-        let f1 = L_date+"."+photo.mimetype.split('/')[1];
-        redirect(null,f1);
-        photoname = f1 ;
+        let f1 = L_date + "." + photo.mimetype.split('/')[1];
+        redirect(null, f1);
+        photoname = f1;
     }
 });
 
-const upload = mult({storage : mystorge});
+const upload = mult({ storage: mystorge });
 
 
-VerifierToken = (req,res,next)=>{
-    let token = req.headers.autorization;
-    if (!token){
-        res.send("Acces Rejected !!!")
-    }
-    try {
-        jwt.verify(token,"24884920");
-        next();
-    } catch (error) {
-        res.send(error)
-    }
+VerifierToken = (req, res, next) =>
+ { let token = req.headers.authorization;
+     if (!token) {
+         res.send(" Acces rejected  !!! Bara ched darkom ")
+         } try {
+             jwt.verify(token, user.Mot_De_Pass);
+              next(); 
+            } catch (error) {
+                 res.status(500).send(error) 
+                } }
+                
+//_____________________________________________________________________________________________________
+
+router_Etudiant.post("/InscriptionEtudiant", upload.any('img'), (req, res) => {
+
+    data = req.body;
+    cle = cryptage.genSaltSync(10);
+    passwordCrypter = cryptage.hashSync(data.Mot_De_Pass, cle);
+    data.Mot_De_Pass = passwordCrypter;
+    po = new N_Etudiant(data);
+    po.image = photoname;
+    po.save().then(() => {
+        photoname = "";
+        res.status(200).send(po);
+    }).catch(() => {
+        res.status(400).send(error);
+    });
+
 }
-router_Etudiant.post("/InscriptionEtudiant", upload.any('img'), (req,res)=>{
-    
-        data = req.body;
-        cle = cryptage.genSaltSync(10);
-        passwordCrypter = cryptage.hashSync("data.Mot_De_Pass",cle);
-        data.Mot_De_Pass = passwordCrypter ;
-        po = new N_Etudiant(data);
-        po.image = photoname ;
-        po.save().then(()=>{
-                photoname = "";
-                res.status(200).send(po);
-        }).catch(()=>{
-                 res.status(400).send(error);
-        });
-        
-    }
 );
 
-router_Etudiant.post("/SignIn",(req,res)=>{
+router_Etudiant.post("/SignIn", (req, res) => {
     data = req.body;
-  
-    
+
+
 });
 
 
-router_Etudiant.get("/Lister",VerifierToken,(req,res)=>{
-    N_Etudiant.find().then((result)=>{
+router_Etudiant.get("/Lister", VerifierToken, (req, res) => {
+    N_Etudiant.find().then((result) => {
         res.send(result);
-    }).catch(()=>{
+    }).catch(() => {
         res.send('error');
     });
 });
 
 
- router_Etudiant.post("/Connexion", async (req,res)=>{
+router_Etudiant.post("/SIGN", async (req, res) => {
     data = req.body;
-   user = await N_Etudiant.findOne({Email : data.Email})
-    if (!user){
+    user = await N_Etudiant.findOne({ Email: data.Email })
+    if (!user) {
         res.status(401).send("Email Icorrect");
-    }else{
-        cryptage.compare(data.Mot_De_Pass,user.Mot_De_Pass).then((Passe)=>{
+    } else {
+        verifPass = cryptage.compareSync(data.Mot_De_Pass, user.Mot_De_Pass);
+        if (!verifPass) {
+            res.status(402).send("Password Icorrect");
+        } else {
+            payload = {
+                id: user._id,
+                NomPrenom: user.NomPrenom,
+                image: user.image
 
-            if(!Passe){
-                res.send("Mot de Passe Icorrect");
-            }else{
-                payload = {
-                    __id : user._id ,
-                    email : user.Email ,
-                    motdepasse : user.NomPrenom
-                }
-               let userToken = jwt.sign(payload,'24884920');
-                res.status(200).send({mytoken : userToken})
             }
-
-        });
+            tokenE = jwt.sign(payload, user.Mot_De_Pass, { expiresIn: "1h" });
+            res.status(200).send({ MyToken: tokenE });
+            localStorage.setItem({ MyToken: tokenE });
+            console.log(localStorage.getItem(MyToken))
+        }
     }
 });
-    
-   
 
 
-router_Etudiant.put("/Modifier",(req,res)=>{});
-router_Etudiant.delete("/Supprimer",(req,res)=>{});
-module.exports=router_Etudiant;
+
+
+
+router_Etudiant.put("/Modifier", (req, res) => { });
+router_Etudiant.delete("/Supprimer", (req, res) => { });
+module.exports = router_Etudiant;
